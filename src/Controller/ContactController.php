02c893c9c6ff2e57aa8a336e6b745724bacc2ff9
Angelique Vannespenne <angeliquevannespenne@hotmail.fr>
2019-07-22 14:43:15 +0200

    update contact :rabbit: :carrot:

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,6 +3,10 @@
 namespace App\Controller;
 
 use App\Form\ContactType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use phpDocumentor\Reflection\Types\Object_;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -13,10 +17,26 @@ class ContactController extends AbstractController
     /**
      * @Route("/contact", name="contact")
      */
-    public function index() :Response
+    public function index(Request $request) :Response
     {
-        return $this->render('contact.html.twig', [
+        $form = $this->createFormBuilder()
+            ->add('name', TextType::class)
+            ->add('mail', EmailType::class)
+            ->add('message', TextType::class)
+            ->add('send', SubmitType::class)
+            ->getForm();
 
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $contact = $form->getData();
+
+            return $this->redirectToRoute('send');
+        }
+
+        return $this->render('contact.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
